74558a7807f3142107d94eac3523e5f8e64d6e59
Lukáš Lalinský <lalinsky@gmail.com>
2019-06-08 17:03:52 +0200

    Wait for the correct PG server

--- admin/ci/run-tests.sh
+++ admin/ci/run-tests.sh
@@ -3,7 +3,8 @@
 set -ex
 
 /wait-for-it.sh $ACOUSTID_TEST_REDIS_HOST:6379
-/wait-for-it.sh $ACOUSTID_TEST_POSTGRES_HOST:5432
+/wait-for-it.sh $ACOUSTID_TEST_DB_DEFAULT_HOST:5432
+/wait-for-it.sh $ACOUSTID_TEST_DB_SLOW_HOST:5432
 
 export PIP_CACHE_DIR=`pwd`/pip-cache
 
