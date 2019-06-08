3e46ea58918c8f9de84abb6d650a1a4450de2fac
Lukáš Lalinský <lalinsky@gmail.com>
2019-06-08 20:23:48 +0200

    Add acoustid-index service

--- admin/ci/run-tests.sh
+++ admin/ci/run-tests.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+/wait-for-it.sh $ACOUSTID_TEST_INDEX_HOST:6080
 /wait-for-it.sh $ACOUSTID_TEST_REDIS_HOST:6379
 /wait-for-it.sh $ACOUSTID_TEST_DB_DEFAULT_HOST:5432
 /wait-for-it.sh $ACOUSTID_TEST_DB_SLOW_HOST:5432
